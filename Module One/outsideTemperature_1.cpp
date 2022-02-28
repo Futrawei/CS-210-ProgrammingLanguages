bcb990a49a7a18b8a0e9c4d11f5b888783c6ed11
Travis <71726772+Futrawei@users.noreply.github.com>
2022-02-28 13:43:09 -0500

    Update and rename outsideTemperature.cpp to outsideTemperature_1.cpp

--- Module One/outsideTemperature_1.cpp
+++ Module One/outsideTemperature_1.cpp
@@ -1,3 +1,6 @@
+// Write a single statement that prints outsideTemperature with 4 digits. End with newline. Sample output with input 103.45632:
+// 103.5
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
