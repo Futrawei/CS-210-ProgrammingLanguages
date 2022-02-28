6b487e67c62e2b532ad0bb9470ee269056c121d7
Travis <71726772+Futrawei@users.noreply.github.com>
2022-02-28 13:30:14 -0500

    Create outsideTemperature.cpp

---
+++ Module One/outsideTemperature.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   double outsideTemperature;
+
+   cin >> outsideTemperature;
+
+   cout << setprecision(4) << outsideTemperature << endl;
+
+   return 0;
+}
