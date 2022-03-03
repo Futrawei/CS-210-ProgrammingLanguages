ddf41cc89991ea06f24af3969f1a8f041787da12
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-03 17:55:17 -0500

    Create finalResult.cpp

---
+++ Module Two/finalResult.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+int main() {
+   int num1;
+   int num2;
+   int finalResult;
+
+   cin >> num1;
+   cin >> num2;
+
+   finalResult = (num1 + num2) / 3;
+
+   cout << "Final result: " << finalResult << endl;
+
+   return 0;
+}
