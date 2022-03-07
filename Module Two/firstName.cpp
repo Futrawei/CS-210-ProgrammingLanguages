3068111c3cb19834bafaeb23001ff0c1856d5f40
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-07 16:53:29 -0500

    Create firstName.cpp

---
+++ Module Two/firstName.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string firstName;
+   string lastName;
+   cin >> firstName;
+   cin >> lastName;
+   
+   cout << lastName << ", " << firstName << "\n";
+ 
+
+   return 0;
+}
