db4714c599ec9779bd12472f1fceaaee07e53f53
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-14 15:01:38 -0400

    Create firstLetter.cpp

---
+++ Module Three/String Access Operations/firstLetter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string userInput;
+   char firstLetter;
+
+   getline(cin, userInput);
+   cin >> firstLetter;
+
+   if (userInput.at(0) == firstLetter) {
+      cout << "Found match: " << firstLetter << endl;
+   }
+   else {
+      cout << "No match: " << firstLetter << endl;
+   }
+
+   return 0;
+}
