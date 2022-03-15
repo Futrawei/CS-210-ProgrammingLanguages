a5f778d49b930831f26dc46e0b034dee84131ebb
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-15 09:59:09 -0400

    Create keepGoing.cpp

---
+++ Module Three/While Loops/keepGoing.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   char keepGoing;
+   int nextBid;
+
+   nextBid = 0;
+   keepGoing = 'y';
+
+   while (keepGoing != 'n') {
+      nextBid = nextBid + 3;
+      cout << "I'll bid $" << nextBid << "!" << endl;
+      cout << "Continue bidding? (y/n) ";
+      cin >> keepGoing;
+   }
+   cout << endl;
+
+   return 0;
+}
