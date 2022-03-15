10895bcfd05c994adceca31ef30f10ff0b1f0bf2
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-15 11:47:58 -0400

    Create countNum.cpp

---
+++ Module Three/While Loops/countNum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int countNum;
+   int i;
+
+   cin >> countNum;
+	cout << "Ready!"<< endl;
+	for(i = countNum; i > 0; i--){
+		cout << i << endl;
+	}
+	cout << "Run!";
+	cout << endl;
+      
+
+   return 0;
+}
