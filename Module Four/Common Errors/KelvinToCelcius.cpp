348fa4cd312bd6871be103b9d82289a370902b9f
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-20 13:19:04 -0400

    Create KelvinToCelcius.cpp

---
+++ Module Four/Common Errors/KelvinToCelcius.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+double CelsiusToKelvin(double valueCelsius) {
+   double valueKelvin;
+
+   valueKelvin = valueCelsius + 273.15;
+
+   return valueKelvin;
+}
+   double KelvinToCelsius(double valueKelvin){
+	double valueCelsius = valueKelvin - 273.15;
+	return valueCelsius;
+   }
+
+
+int main() {
+   double valueC;
+   double valueK;
+
+   valueC = 10.0;
+   cout << valueC << " C is " << CelsiusToKelvin(valueC) << " K" << endl;
+
+   cin >> valueK;
+   cout << valueK << "  is " << KelvinToCelsius(valueK) << " C" << endl;
+
+   return 0;
+}
