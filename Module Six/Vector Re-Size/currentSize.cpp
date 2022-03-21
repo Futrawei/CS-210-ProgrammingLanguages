d9e2a16d33953c9a904182bc93a508967af21625
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-21 12:31:30 -0400

    Create currentSize.cpp

---
+++ Module Six/Vector Re-Size/currentSize.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+   vector<int> sensorReadings(4);
+   int currentSize;
+   int input;
+   cin >> input;
+
+   sensorReadings.resize(input);
+
+   currentSize = sensorReadings.size();
+   
+
+   cout << "Number of elements: " << currentSize << endl;
+
+   return 0;
+}
