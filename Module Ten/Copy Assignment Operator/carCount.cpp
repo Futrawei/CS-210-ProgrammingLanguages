fa8136ddd4263a3a7ae2fd2c82410ba78c2f00e5
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-28 15:48:49 -0400

    Create carCount.cpp

---
+++ Module Ten/Copy Assignment Operator/carCount.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class CarCounter {
+   public:
+      CarCounter();
+      ~CarCounter();
+      CarCounter& operator=(const CarCounter& objToCopy);
+      void SetCarCount(const int setVal) {
+         *carCount = setVal;
+      }
+      int GetCarCount() const {
+         return *carCount;
+      }
+   private:
+      int* carCount;
+};
+
+CarCounter::CarCounter() {
+   carCount = new int;
+   *carCount = 0;
+}
+
+CarCounter::~CarCounter() {
+   delete carCount;
+}
+
+// FIXME write copy assignment operator
+CarCounter& CarCounter::operator = (const CarCounter& objToCopy) {
+   *carCount = objToCopy.GetCarCount();
+   return *this;
+}
+
+
+int main() {
+   CarCounter frontParkingLot;
+   CarCounter backParkingLot;
+   int count;
+
+   cin >> count;
+
+   frontParkingLot.SetCarCount(count);
+   backParkingLot = frontParkingLot;
+
+   cout << "Cars counted: " << backParkingLot.GetCarCount();
+
+   return 0;
+}
