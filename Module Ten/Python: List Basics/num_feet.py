f7ede068bee33064a5a41325c6e83557a1e6fe16
Travis <71726772+Futrawei@users.noreply.github.com>
2022-03-29 11:43:24 -0400

    Update num_feet.py

--- Module Ten/Python: List Basics/num_feet.py
+++ Module Ten/Python: List Basics/num_feet.py
@@ -1,6 +1,6 @@
 
 def print_total_inches(num_feet, num_inches):
-    print("Total inches: " +str(num_feet * 12 + num_inches))
+    print("Total inches: " + str(num_feet * 12 + num_inches))
 
 feet = int(input())
 inches = int(input())
